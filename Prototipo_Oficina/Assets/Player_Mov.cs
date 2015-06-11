212cee2e17bb20b21ebc139c58d5bf7c9f283c39
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-11 20:37:26 -0300

    Movimentação desgovernada
    Ajustar o clique ao mundo

---
+++ Prototipo_Oficina/Assets/Player_Mov.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Mov : MonoBehaviour {
+		
+	private Vector2 startPosition;
+	private Vector2 finalPosition;
+	private float speed;
+	private bool isMoving;
+
+	//transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+	// Use this for initialization
+	void Start () {
+		speed = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMoving) {
+			if (Input.GetMouseButtonDown (0)) {
+				startPosition = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+				finalPosition = Input.mousePosition;
+				isMoving = true;
+			}
+		} else if (isMoving) {
+			gameObject.transform.position = Vector2.MoveTowards (startPosition, finalPosition, Time.deltaTime * speed);
+			startPosition = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+			isMoving = startPosition == finalPosition? isMoving = false : isMoving = true;
+		}
+
+	}
+}
+
+
+//Camera camera = GetComponent<MainCamera>();
+//Vector3 p = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
