2ac4aeeae8b126d06156e6ffd29808946b9c42f8
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-14 16:34:31 -0300

    Mudando no lugar certo!
    Sim, nas portas! E com uma boa cancela, diga-se de passagem.

--- Prototipo_Oficina/Assets/Scripts/DoorBehaviour.cs
+++ Prototipo_Oficina/Assets/Scripts/DoorBehaviour.cs
@@ -4,24 +4,38 @@ using System.Collections;
 public class DoorBehaviour : MonoBehaviour {
 
 	public Texture2D cursor;
-	public GameObject player;
 	public GUITexture Fader;
 	public string Destination;
+	private bool canChangeScene = false;
+	private bool isWorking = false;
 
 	void OnMouseOver(){
 		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
-		if (Input.GetMouseButtonDown (0)) {
+		isWorking = false;
+		if (Input.GetMouseButtonDown (0))
+			canChangeScene = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		if(col.gameObject.tag == "Player" && canChangeScene)
 			StartCoroutine(ChangeScene());
-		}
 	}
 
 	IEnumerator ChangeScene(){
+		yield return new WaitForSeconds(.23f);
 		Instantiate (Fader);
 		yield return new WaitForSeconds(.5f);
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 		Application.LoadLevel(Destination);
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0) && isWorking)
+			canChangeScene = false;
 	}
 	
 	void OnMouseExit(){
 		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+		isWorking = true;
 	}
 }
