cd3e4afa3ac64ba657b0c26f1cc8de7d88766782
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-14 04:04:50 -0300

    Hora de mudar de cena!
    Com estilo =]

---
+++ Prototipo_Oficina/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBehaviour : MonoBehaviour {
+
+	public Texture2D cursor;
+	public GameObject player;
+	public GUITexture Fader;
+
+	void OnMouseOver(){
+		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+		if (Input.GetMouseButtonDown (0)) {
+			//Application.LoadLevel(gameObject.name);
+			StartCoroutine(ChangeScene());
+		}
+	}
+
+	IEnumerator ChangeScene(){
+		Instantiate (Fader);
+		yield return new WaitForSeconds(.5f);
+		Application.LoadLevel(gameObject.name);
+	}
+	
+	void OnMouseExit(){
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+}
--- Prototipo_Oficina/Assets/Scripts/Player_Mov.cs
+++ Prototipo_Oficina/Assets/Scripts/Player_Mov.cs
@@ -7,33 +7,30 @@ public class Player_Mov : MonoBehaviour {
 	private Vector2 finalPosition;
 	private float speed;
 	private bool isMoving;
-
-	//transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+	
 	// Use this for initialization
 	void Start () {
-		speed = 10;
+		speed = 3;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!isMoving) {
-			if (Input.GetMouseButtonDown (0)) {
+
+	void goToMouse(){
+		if (Input.GetMouseButtonDown (0)) {
+			if (!isMoving) {
 				startPosition = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
-                finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				isMoving = true;
 			}
+
+			finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			
 		} else if (isMoving) {
-            if (Input.GetMouseButtonDown(0)) {
-                finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            }
 			gameObject.transform.position = Vector2.MoveTowards (startPosition, finalPosition, Time.deltaTime * speed);
 			startPosition = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
 			isMoving = startPosition == finalPosition? isMoving = false : isMoving = true;
 		}
-
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		goToMouse ();
 	}
 }
-
-
-//Camera camera = GetComponent<MainCamera>();
-//Vector3 p = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
