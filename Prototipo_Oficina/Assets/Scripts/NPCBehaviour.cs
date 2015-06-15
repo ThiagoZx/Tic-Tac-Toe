8a9cf0cda3fba1371753b74ae4ede508d1fbb521
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-15 14:34:40 -0300

    Deixa pra falar!
    Mas ainda sem palavras

---
+++ Prototipo_Oficina/Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCBehaviour : MonoBehaviour {
+
+	public Texture2D cursor;
+
+	void OnMouseOver(){
+		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+	}
+	
+	void OnMouseExit(){
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+}
