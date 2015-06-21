af682658a128f329c35febe5dbe010f06d4a9b8d
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-21 15:21:28 -0300

    Sobre mudanças.
    Não tem nenhuma relevante

--- Prototipo_Oficina/Assets/Scripts/NPCBehaviour.cs
+++ Prototipo_Oficina/Assets/Scripts/NPCBehaviour.cs
@@ -4,9 +4,35 @@ using System.Collections;
 public class NPCBehaviour : MonoBehaviour {
 
 	public Texture2D cursor;
-
+	public GameObject textBox;
+	public GameObject[] scripts;
+	public string[] text;
+  
 	void OnMouseOver(){
 		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+		if (Input.GetMouseButtonDown (0)) {
+			chatIn();
+		}
+	}
+
+	private void chatIn(){
+		for (int i = 0; i < scripts.Length; i++) {
+			if(scripts[i].tag == "Player"){
+				scripts[i].GetComponent<Player_Mov>().enabled = false;
+			} else if(scripts[i].tag == "Door"){
+				scripts[i].GetComponent<DoorBehaviour>().enabled = false;
+			}
+		}
+	}
+
+	private void chatOut(){
+		for (int i = 0; i < scripts.Length; i++) {
+			if(scripts[i].tag == "Player"){
+				scripts[i].GetComponent<Player_Mov>().enabled = true;
+			} else if(scripts[i].tag == "Door"){
+				scripts[i].GetComponent<DoorBehaviour>().enabled = true;
+			}
+		}
 	}
 	
 	void OnMouseExit(){
