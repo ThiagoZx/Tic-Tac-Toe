80eab53a8c45515f9995cf1a5d7fa7bef4684cf8
Thiago Torres <TheThiagoo0@gmail.com>
2015-08-10 15:07:06 -0300

    Interaction please.
    To NOTHINGNESS.

---
+++ Prototipo_Oficina/Assets/Scripts/InteractionBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionBehaviour : MonoBehaviour {
+	
+	public Texture2D cursor;
+	
+	void OnMouseOver(){
+		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+		if (Input.GetMouseButtonUp (0)) {
+			print("Ahooy");
+		}
+	}
+
+	void OnMouseExit(){
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+
+}
