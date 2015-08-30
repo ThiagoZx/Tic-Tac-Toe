6df10abe5bfc83c25f8591cf8af26dc52a245c21
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-30 17:27:33 -0300

    Portas de volta!
    Chave sem ir em direção, portas devidamente funcionando. Game Controller
    com suas funções, esperando mais funcionalidades a partir de tags

--- Prototipo_Oficina/Assets/Scripts/GameController.cs
+++ Prototipo_Oficina/Assets/Scripts/GameController.cs
@@ -8,12 +8,12 @@ public class GameController : MonoBehaviour {
 
 			Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.up, 0.0023f);
-			print(hit.transform.gameObject.tag.ToString());
 
 			switch (hit.transform.gameObject.tag) {
 				
 			case "Item":
 				hit.transform.gameObject.GetComponent<ItemBehaviour>().changePlace(hit.transform.gameObject);
+				GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Mov>().moveAllowed = false;
 				break;
 
 			case "Floor":
@@ -21,7 +21,6 @@ public class GameController : MonoBehaviour {
 				break;
 
 			case "Door":
-				print("Zup");
 				if(!hit.transform.gameObject.GetComponent<DoorBehaviour>().DoorOpen){
 					hit.transform.gameObject.GetComponent<DoorBehaviour>().DoorOpen = true;
 					//Criar funçao que muda a imagem da porta para aberta e faz um simpatico som dela abrindo.
--- Prototipo_Oficina/Assets/Scripts/ItemBehaviour.cs
+++ Prototipo_Oficina/Assets/Scripts/ItemBehaviour.cs
@@ -8,10 +8,6 @@ public class ItemBehaviour : MonoBehaviour {
 
 	void OnMouseOver(){
 		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
-		if (Input.GetMouseButtonDown (0)) {
-			//Lembrar de fazer isso modular ao interagir um objeto com outro. Pensei em usar um raycast para isso
-			changePlace(gameObject);
-		}
 	}
 
 	public void changePlace(GameObject item){
